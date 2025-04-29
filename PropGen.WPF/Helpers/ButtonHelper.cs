72f3b2bf097b4ca670f5dfa034076093062300f0
git-dibidus <dxzisk98@hotmail.com>
2025-04-29 11:09:50 -0400

    Added ButtonHelper

---
+++ PropGen.WPF/Helpers/ButtonHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace PropGen.WPF.Helpers
+{
+    public static class ButtonHelper
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "CornerRadius",
+                typeof(CornerRadius),
+                typeof(ButtonHelper),
+                new FrameworkPropertyMetadata(new CornerRadius(0), FrameworkPropertyMetadataOptions.Inherits)
+            );
+
+        public static void SetCornerRadius(UIElement element, CornerRadius value)
+        {
+            element.SetValue(CornerRadiusProperty, value);
+        }
+
+        public static CornerRadius GetCornerRadius(UIElement element)
+        {
+            return (CornerRadius)element.GetValue(CornerRadiusProperty);
+        }
+    }
+}
